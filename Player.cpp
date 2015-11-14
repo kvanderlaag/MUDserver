5bd98f9ffcbcc630aa1170378f7d600cf3dd75ef
toddxu <Todd@w87-168-072.wireless.uvic.ca>
2015-11-14 15:26:12 -0800

    more functions for player
    added password, room ID, and items list.
    
    Player can be assigned room ID and connection ID. Player also has
    password checking.

--- Player.cpp
+++ Player.cpp
@@ -1,23 +1,60 @@
 #include "Player.h"
 
-Player::Player(int id, std::string name) : GameEntity(id, name){
+Player::Player( int id, std::string name ) : GameEntity(id, name){
     std::cout << "Created a player..." << std::endl;
 }
 
-Player::Player(int id, std::string name, int connectionID) : GameEntity( id, name ) {
-    connectionID_ = connectionID;
-    std::cout << "Created a player with connection ID..." << std::endl;
+Player::Player(  int id, std::string name, int password ) : GameEntity( id, name ) {
+    password_ = password;
+    std::cout << "Created a player with connection ID & password..." << std::endl;
 }
 
 Player::~Player() {
     std::cout << "Removed a player..." << std::endl;
 }
 
-int Player::GetConnectionID() {
+void Player::AssignConnectionID(int* newConnectionID){
+    *connectionID_ = *newConnectionID;
+}
+
+int* Player::GetConnectionID() {
     return connectionID_;
 }
 
+int Player::GetPassword() {
+    return password_;
+}
+
+int* Player::GetRoomID(){
+    return roomID_;
+}
+
+void Player::ChangeRoomID(int* newRoomID){
+    *roomID_ = *newRoomID;
+}
+
 void Player::PrintPlayer(){
-	Print();
-    std::cout << connectionID_ << std::endl;
+    Print();
+    std::cout << connectionID_ << "is currently located at room:" << roomID_ << std::endl;
+}
+
+bool Player::CheckPassword(int inputPassword) {
+	if(inputPassword == password_){
+        return true;
+        std::cout << "Password is correct..." << std::endl;
+    }
+    return false;
+    std::cout << "Incorrect password..." << std::endl; 
+}
+
+void Player::AddItem(GameEntity *item) {
+	items_->AddEntity(item);
+}
+
+void Player::RemoveItem(int id) {
+	items_->RemoveEntity(id);
+}
+
+GameEntity* Player::GetItem(int id) {
+	return items_->GetEntity(id);
 }
--- Player.h
+++ Player.h
@@ -2,21 +2,35 @@
 #define __PLAYER_H__
 
 #include "GameEntity.h"
+#include "EntityList.h"
 #include <iostream>
 #include <string>
 
 class Player: public GameEntity{
-
+    
 private:
-    int connectionID_;
-
+    int* connectionID_;
+    int password_;
+    int* roomID_;
+    EntityList* items_;
+    
 public:
-    Player(int id, std::string name);
-	Player(int id, std::string name, int connectionID_);
+    Player( int id, std::string name);
+    Player( int id, std::string name, int password);
     ~Player();
-	void PrintPlayer();
-
-	int GetConnectionID();
+    void PrintPlayer();
+    
+    void AssignConnectionID(int* newConnectionID);
+    int* GetConnectionID();
+    int* GetRoomID();
+    bool CheckPassword(int inputPassword);
+    void ChangeRoomID(int* newRoomID);
+    void AddItem(GameEntity* entity);
+	void RemoveItem(int id);
+	GameEntity *GetItem(int id);
+    
+protected:
+    int GetPassword();
 };
 
 #endif 
