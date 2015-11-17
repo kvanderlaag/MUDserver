dcdb33780979f790d5f67d68ebe920e1f8ae7295
speggos <spencercoxcs@gmail.com>
2015-11-17 14:05:17 -0800

    Update GameEntity.cpp

--- GameEntity.cpp
+++ GameEntity.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-GameEntity::GameEntity(int id, std::string name) {
+GameEntity::GameEntity(int id, std::string name) {				/*Create a game entity*/
     id_ = id;
     name_ = name;
     description_ = "";
@@ -10,7 +10,7 @@ GameEntity::GameEntity(int id, std::string name) {
     std::cout << "Created an entity..." << std::endl;
 }
 
-GameEntity::GameEntity(int id, std::string name, std::string description) {
+GameEntity::GameEntity(int id, std::string name, std::string description) {	/*Creates a game entity with a description*/
     id_ = id;
     name_ = name;
     description_ = description;
@@ -18,26 +18,26 @@ GameEntity::GameEntity(int id, std::string name, std::string description) {
     std::cout << "Created an entity..." << std::endl;
 }
 
-GameEntity::~GameEntity() {
+GameEntity::~GameEntity() {							/*Remove an entity*/
     std::cout << "Destroyed an entity..." << std::endl;
 }
 
-int GameEntity::GetId() {
+int GameEntity::GetId() {							/*Get the Id of an entity*/
     return id_;
 }
 
-std::string GameEntity::GetName() {
+std::string GameEntity::GetName() {						/*Get the name of an entity*/
 	return name_;
 }
 
-std::string GameEntity::GetDescription() {
+std::string GameEntity::GetDescription() {					/*Get the description of an entity*/
     return description_;
 }
 
-void GameEntity::SetDescription(std::string description) {
+void GameEntity::SetDescription(std::string description) {			/*Change or set the description of an entity*/
     description_ = description;
 }
 
-void GameEntity::Print() {
+void GameEntity::Print() {							/*Print the ID, name, and description of an entity*/
     std::cout << id_ << " " << name_ << " " << description_ << std::endl;
 }
