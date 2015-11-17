367fa174fa8f74a132912bbfabf208e76c09a15b
speggos <spencercoxcs@gmail.com>
2015-11-17 13:59:50 -0800

    Update EntityList.cpp

--- EntityList.cpp
+++ EntityList.cpp
@@ -2,15 +2,15 @@
 
 #include <iostream>
 
-EntityList::EntityList() {
+EntityList::EntityList() {						/*Create an entity list*/
     std::cout << "Created a list..." << std::endl;
 }
 
-EntityList::~EntityList() {
+EntityList::~EntityList() {						/*Delete an entity list*/
     std::cout << "Destroyed a list..." << std::endl;
 }
 
-void EntityList::AddEntity(GameEntity *game_entity) {
+void EntityList::AddEntity(GameEntity *game_entity) {			/*Add a game entity to an existing list*/
 
 	if( map_.find( game_entity->GetId() ) != map_.end() ){
         std::cout << "Duplicate id!!!" << std::endl;
@@ -19,14 +19,14 @@ void EntityList::AddEntity(GameEntity *game_entity) {
     map_.insert( std::pair<int,GameEntity*>( game_entity->GetId(), game_entity ) );
 }
 
-GameEntity* EntityList::GetEntity(int id) {
+GameEntity* EntityList::GetEntity(int id) {				/*Get the entity corresponding to an ID*/
     if( !map_.empty() ) {
         return map_.find( id )->second;
     }
     return NULL;
 }
 
-void EntityList::RemoveEntity(int id) {
+void EntityList::RemoveEntity(int id) {					/*Remove an entity corresponding to an ID*/
     if( !map_.empty() ) {
         if( map_.find(id) != map_.end() )
             map_.erase(id);
@@ -35,6 +35,6 @@ void EntityList::RemoveEntity(int id) {
     }
 }
 
-int EntityList::GetEntityCount() {
+int EntityList::GetEntityCount() {					/*Return how many entities are in the list*/
     return map_.size();
 }
