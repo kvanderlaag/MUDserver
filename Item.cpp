c9d8745087469143a126787235ada81ff0360d29
speggos <spencercoxcs@gmail.com>
2015-11-17 14:39:39 -0800

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -1,13 +1,13 @@
 #include "Item.h"
 
-Item::Item(int id, std::string description) : GameEntity(id, description) {
+Item::Item(int id, std::string description) : GameEntity(id, description) {				/*Create an item with a description*/
 	std::cout <<"Created an item..." << std::endl;
 }
 
-Item::Item(int id, std::string name, std::string description) : GameEntity(id, name, description) {
+Item::Item(int id, std::string name, std::string description) : GameEntity(id, name, description) {	/*Create an item with a name and description*/
 	std::cout << "Created an item..." << std::endl;
 }
 
-Item::~Item() {
+Item::~Item() {												/*Destroy an item*/
 	std::cout << "Destroyed an item..." << std::endl;
 }
